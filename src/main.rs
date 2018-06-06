b0f050667aaf2f921226fa28270f7483615980a5
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-06 21:08:50 +0200

    make executable not panic if version not found

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate solc;
 
 fn main() {
     println!("solc available = {}", solc::is_solc_available());
-    println!("solc version = {}", solc::solc_version().unwrap());
+    println!("solc version = {:?}", solc::solc_version());
     println!("");
     println!("solcjs available = {}", solc::is_solcjs_available());
-    println!("solcjs version = {}", solc::solcjs_version().unwrap());
+    println!("solcjs version = {:?}", solc::solcjs_version());
 }
