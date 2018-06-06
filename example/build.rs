d9a84716b010e9c0fe0444079900503f160a422c
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-06 19:56:21 +0200

    add build.rs to example

---
+++ example/build.rs
@@ -0,0 +1,9 @@
+extern crate solc;
+
+fn main() {
+    // always rerun build script if contract has changed
+    println!("cargo:rerun-if-changed=./contracts/test.sol");
+
+    // println!("cargo:rustc-env=SOLC_VERSION={}", solc::solc_version);
+    solc::solc_compile("./contracts/test.sol", "./contracts").unwrap();
+}
