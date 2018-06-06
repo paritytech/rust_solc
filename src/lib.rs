d7c96ad8c47fb5a8a346b0fe268ef20f87372268
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-06 15:05:12 +0200

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -14,17 +14,21 @@ pub fn is_solc_available() -> bool {
 /// returns the output of `solc --version`.
 /// more specifically the last line which is the version string.
 pub fn solc_version() -> error::Result<String> {
-    let output = Command::new("solc").args(&["--version"]).output().chain_err(|| "failed to run `solc --version`")?;
-    let output_string = String::from_utf8(output.stdout).chain_err(|| "output from `solc --version` is not utf8")?;
-    let version = output_string.lines().last().chain_err(|| "output from `solc --version` is empty")?.to_owned();
+    let output = Command::new("solc")
+        .args(&["--version"])
+        .output()
+        .chain_err(|| "failed to run `solc --version`")?;
+    let output_string =
+        String::from_utf8(output.stdout).chain_err(|| "output from `solc --version` is not utf8")?;
+    let version = output_string
+        .lines()
+        .last()
+        .chain_err(|| "output from `solc --version` is empty")?
+        .to_owned();
     Ok(version)
 }
 
 /// returns whether `solcjs` is in path
-pub fn is_solcjs_available() {
+pub fn is_solcjs_available() {}
 
-}
-
-pub fn compile_with_solc() {
-
-}
+pub fn compile_with_solc() {}
