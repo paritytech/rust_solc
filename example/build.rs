d129dc8374918a19e6f8561baa4ae1fd6638af8d
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-06 21:16:30 +0200

    remove outcommented code from example build.rs

--- example/build.rs
+++ example/build.rs
@@ -4,6 +4,5 @@ fn main() {
     // always rerun build script if contract has changed
     println!("cargo:rerun-if-changed=./contracts/test.sol");
 
-    // println!("cargo:rustc-env=SOLC_VERSION={}", solc::solc_version);
     solc::compile("./contracts/test.sol", "./contracts").unwrap();
 }
