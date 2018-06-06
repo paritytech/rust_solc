d67a750428de6065715937590273d711ee1c53b9
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-06 19:56:47 +0200

    use ethabi in example

---
+++ example/src/lib.rs
@@ -0,0 +1,8 @@
+extern crate ethabi;
+#[macro_use]
+extern crate ethabi_derive;
+#[macro_use]
+extern crate ethabi_contract;
+
+use_contract!(sender_test, "GetSenderTest", "./contracts/GetSenderTest.abi");
+use_contract!(value_test, "GetValueTest", "./contracts/GetValueTest.abi");
