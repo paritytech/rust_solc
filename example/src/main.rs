5e8b51890ac43694e4bd65974b268602aadda5a5
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-06 15:12:23 +0200

    update example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,9 @@
 extern crate solc;
 
 fn main() {
-    println!("{}", solc::is_solc_available());
-    println!("{}", solc::solc_version().unwrap());
+    println!("solc available = {}", solc::is_solc_available());
+    println!("solc version = {}", solc::solc_version().unwrap());
+    println!("");
+    println!("solcjs available = {}", solc::is_solcjs_available());
+    println!("solcjs version = {}", solc::solcjs_version().unwrap());
 }
