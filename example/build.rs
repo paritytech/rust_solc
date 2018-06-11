cdcd7cb94b5d60e548522e9be7bdf5846b00b6f1
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-11 18:41:32 +0200

    go back to building example on travis

--- example/build.rs
+++ example/build.rs
@@ -4,5 +4,5 @@ fn main() {
     // always rerun build script if contract has changed
     println!("cargo:rerun-if-changed=./contracts/test.sol");
 
-    // solc::common_compile("./contracts", "./contracts").unwrap();
+    solc::compile_dir("./contracts", "./contracts").unwrap();
 }
