ed13d3ecfa3cb1c4e2c6dc54670257274ae88db1
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-11 21:56:04 +0200

    add and use fn rename_solcjs_outputs

--- src/lib.rs
+++ src/lib.rs
@@ -75,9 +75,10 @@ pub fn compile_dir<A: AsRef<Path>, B: AsRef<Path>>(
 
     for input_file_path in solidity_file_paths(input_dir_path)? {
         if is_solc_available {
-            solc_compile(input_file_path, &output_dir_path)?;
+            solc_compile(&input_file_path, &output_dir_path)?;
         } else {
-            solcjs_compile(input_file_path, &output_dir_path)?;
+            solcjs_compile(&input_file_path, &output_dir_path)?;
+            rename_solcjs_outputs(&input_file_path, &output_dir_path)?;
         }
     }
     Ok(())
@@ -153,6 +154,41 @@ pub fn solidity_file_paths<T: AsRef<Path>>(directory: T) -> std::io::Result<Vec<
     }
 
     Ok(results)
+}
+
+/// `solcjs` prefixes output files (one per contract) with the input filename while `solc` does not.
+/// rename files so output files can be found in identical places regardless
+/// of whether `solcjs` or `solc` is used
+pub fn rename_solcjs_outputs<A: AsRef<Path>, B: AsRef<Path>>(
+    input_file_path: A,
+    output_dir_path: B,
+) -> error::Result<()> {
+    let input_file_stem = input_file_path
+        .as_ref()
+        .file_stem()
+        .ok_or(format!("input file path `{:?}` must have a file stem", input_file_path.as_ref()))?
+        .to_str()
+        .ok_or(format!("input file path `{:?}` must be utf8 but isn't", input_file_path.as_ref()))?;
+
+    let prefix = format!("{}_sol_", input_file_stem);
+
+    for maybe_entry in std::fs::read_dir(&output_dir_path)? {
+        let src_path = maybe_entry?.path();
+        if let Some(file_name) = src_path.file_name().map(|x| x.to_os_string()) {
+            let file_name = file_name.to_str()
+                .ok_or(format!("file name `{:?}` in dir `{:?}` must be utf8 but isn't", file_name, output_dir_path.as_ref()))?;
+            if !file_name.starts_with(&prefix) { continue; }
+
+            if let Some(extension) = src_path.extension() {
+                if extension != "abi" && extension != "bin" { continue; }
+            }
+
+            // dst = src path with `prefix` stripped from front of file name
+            let dst_path = src_path.with_file_name(&file_name[prefix.len()..]);
+            std::fs::rename(src_path, dst_path)?;
+        }
+    }
+    Ok(())
 }
 
 /// at the time of writing this is broken for `solcjs`
