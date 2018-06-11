cf19061d6dc4514cfa611fcde0b0a6b7361b39d1
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-11 18:18:00 +0200

    improve order in file, docstring, make fn private

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,58 @@ use std::process::{Command, Output, Stdio};
 pub mod error;
 use error::ResultExt;
 
+/// returns whether `solc` is in path.
+///
+/// `solc` is the C++ implementation of the solidity compiler.
+pub fn is_solc_available() -> bool {
+    solc_version().is_ok()
+}
+
+/// returns whether `solcjs` is in path.
+///
+/// `solcjs` is the javascript implementation of the solidity compiler.
+pub fn is_solcjs_available() -> bool {
+    solcjs_version().is_ok()
+}
+
+/// returns the output of `solc --version`.
+///
+/// more specifically returns the last output line which is the version string.
+/// `solc` is the C++ implementation of the solidity compiler.
+pub fn solc_version() -> error::Result<String> {
+    common_version("solc")
+}
+
+/// returns the output of `solcjs --version`.
+///
+/// more specifically returns the last output line which is the version string.
+/// `solcjs` is the javascript implementation of the solidity compiler.
+pub fn solcjs_version() -> error::Result<String> {
+    common_version("solcjs")
+}
+
+/// version code that's common for `solc` and `solcjs`
+fn common_version(command_name: &str) -> error::Result<String> {
+    let command_output = Command::new(command_name)
+        .arg("--version")
+        .output()
+        .chain_err(|| format!("failed to run `{} --version`", command_name))?;
+    if !command_output.status.success() {
+        return Err(error::ErrorKind::ExitStatusNotSuccess(
+            command_name.to_owned(),
+            command_output.status,
+        ).into());
+    }
+    let stdout = String::from_utf8(command_output.stdout)
+        .chain_err(|| format!("output from `{} --version` is not utf8", command_name))?;
+    let version = stdout
+        .lines()
+        .last()
+        .chain_err(|| format!("output from `{} --version` is empty", command_name))?
+        .to_owned();
+    Ok(version)
+}
+
 /// shells out to either `solc` or `solcjs` (whichever is available in that order)
 /// to compile all solidity files in `input_dir_path`
 /// into abi and bin files in `output_dir_path`.
@@ -31,22 +83,7 @@ pub fn compile<A: AsRef<Path>, B: AsRef<Path>>(
     Ok(())
 }
 
-/// returns whether `solc` is in path.
-///
-/// `solc` is the C++ implementation of the solidity compiler.
-pub fn is_solc_available() -> bool {
-    solc_version().is_ok()
-}
-
-/// returns the output of `solc --version`.
-///
-/// more specifically returns the last output line which is the version string.
-/// `solc` is the C++ implementation of the solidity compiler.
-pub fn solc_version() -> error::Result<String> {
-    common_version("solc")
-}
-
-/// shells out to `solc` to compile
+/// shells out to `solc` to compile the single file at
 /// `input_file_path` into abi and bin files in `output_dir_path`.
 ///
 /// `solc` is the C++ implementation of the solidity compiler.
@@ -74,22 +111,7 @@ pub fn solc_compile<A: AsRef<Path>, B: AsRef<Path>>(
     Ok(command_output)
 }
 
-/// returns whether `solcjs` is in path.
-///
-/// `solcjs` is the javascript implementation of the solidity compiler.
-pub fn is_solcjs_available() -> bool {
-    solcjs_version().is_ok()
-}
-
-/// returns the output of `solcjs --version`.
-///
-/// more specifically returns the last output line which is the version string.
-/// `solcjs` is the javascript implementation of the solidity compiler.
-pub fn solcjs_version() -> error::Result<String> {
-    common_version("solcjs")
-}
-
-/// shells out to `solcjs` to compile
+/// shells out to `solcjs` to compile the single file at
 /// `input_file_path` into abi and bin files in `output_dir_path`.
 ///
 /// `solcjs` is the javascript implementation of the solidity compiler.
@@ -134,28 +156,8 @@ pub fn solidity_file_paths<T: AsRef<Path>>(directory: T) -> std::io::Result<Vec<
     Ok(results)
 }
 
-/// version code that's common for `solc` and `solcjs`
-fn common_version(command_name: &str) -> error::Result<String> {
-    let command_output = Command::new(command_name)
-        .arg("--version")
-        .output()
-        .chain_err(|| format!("failed to run `{} --version`", command_name))?;
-    if !command_output.status.success() {
-        return Err(error::ErrorKind::ExitStatusNotSuccess(
-            command_name.to_owned(),
-            command_output.status,
-        ).into());
-    }
-    let stdout = String::from_utf8(command_output.stdout)
-        .chain_err(|| format!("output from `{} --version` is not utf8", command_name))?;
-    let version = stdout
-        .lines()
-        .last()
-        .chain_err(|| format!("output from `{} --version` is empty", command_name))?
-        .to_owned();
-    Ok(version)
-}
-
+/// at the time of writing this is broken for `solcjs`
+/// due to issue https://github.com/ethereum/solc-js/issues/126
 pub fn standard_json(input_json: &str) -> error::Result<String> {
     let is_solc_available = is_solc_available();
 
@@ -168,7 +170,7 @@ pub fn standard_json(input_json: &str) -> error::Result<String> {
     common_standard_json(command_name, input_json)
 }
 
-pub fn common_standard_json(command_name: &str, input_json: &str) -> error::Result<String> {
+fn common_standard_json(command_name: &str, input_json: &str) -> error::Result<String> {
     let full_command = format!("{} --standard-json", command_name);
 
     let mut process = Command::new(command_name)
