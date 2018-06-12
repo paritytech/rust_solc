4c9b3c84c035f06236f959ebe2d7fb780f4b3f48
Maximilian Krüger <kruemaxi@googlemail.com>
2018-06-12 20:42:32 +0200

    remove src/main.rs which isn't really needed

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate solc;
-
-fn main() {
-    println!("solc available = {}", solc::is_solc_available());
-    println!("solc version = {:?}", solc::solc_version());
-    println!("");
-    println!("solcjs available = {}", solc::is_solcjs_available());
-    println!("solcjs version = {:?}", solc::solcjs_version());
-}
