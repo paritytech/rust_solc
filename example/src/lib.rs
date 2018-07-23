e0d13a1519129d2e8412a88dba02f134211a97b0
Maximilian Krüger <kruemaxi@googlemail.com>
2018-07-23 16:46:40 +0200

    example: adapt to upstream ethabi changes

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -4,9 +4,5 @@ extern crate ethabi_derive;
 #[macro_use]
 extern crate ethabi_contract;
 
-use_contract!(
-    sender_test,
-    "GetSenderTest",
-    "./contracts/GetSenderTest.abi"
-);
-use_contract!(value_test, "GetValueTest", "./contracts/GetValueTest.abi");
+use_contract!(sender_test, "./contracts/GetSenderTest.abi");
+use_contract!(value_test, "./contracts/GetValueTest.abi");
